aef30b493898d66ebfda1f455e2ae50cbd3b151b
Jun Yokoyama <jun@larus.org>
2012-04-18 12:02:50 +0900

    initial commit
    rails new rails_join -d mysql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsJoin::Application.config.secret_token = '0b6f1f6db32a1bb616720b59e6bccc0c717ecf423a73898db275950a3eb0e9c942935491ce8244621b5367aa4b564a5360a7cb0ab17bd71ff69109f225b1ba9b'
